f398540e2384661ccb9d2f4c2e2fef1a6be8352e
poh18 <poh18@DESKTOP-EBI3PKU>
2020-11-09 17:27:28 +0900

    [박용호]cashapi 생성(해당 연도의 월별로 지출을 ajax로 출력), cash프로젝트와 연결

---
+++ cashapi/src/main/java/kr/co/gdu/cashapi/restcontroller/CashStatsRestController.java
@@ -0,0 +1,21 @@
+package kr.co.gdu.cashapi.restcontroller;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kr.co.gdu.cashapi.service.CashStatsService;
+
+@RestController
+public class CashStatsRestController {
+	@Autowired CashStatsService cashStatsService;
+	
+	
+	@GetMapping("/totalOfMonthByYear")
+	public Map<String, Object> totalOfMonthByYear(){
+		Map<String, Object> map = cashStatsService.getTotalOfMonthByYear();
+		return map;// @RestController때문에 객체처리를 JSON문자열로 변경
+	}
+}
