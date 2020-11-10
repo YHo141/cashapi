414f9f8caaa873a8363557b006603d48e0e1a7da
GDJ29 <GDJ29@DESKTOP-5H1FDF5>
2020-11-10 15:02:57 +0900

    [박용호]데이터속성의 배열값을 ajax호출 후 결과값을 채워 차트를 출력(bar, pie)

--- cashapi/src/main/java/kr/co/gdu/cashapi/restcontroller/CashStatsRestController.java
+++ cashapi/src/main/java/kr/co/gdu/cashapi/restcontroller/CashStatsRestController.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import kr.co.gdu.cashapi.service.CashStatsService;
@@ -17,5 +18,11 @@ public class CashStatsRestController {
 	public Map<String, Object> totalOfMonthByYear(){
 		Map<String, Object> map = cashStatsService.getTotalOfMonthByYear();
 		return map;// @RestController때문에 객체처리를 JSON문자열로 변경
+	}
+	
+	@GetMapping("/totalOutAndInByYear/{year}")
+	public Map<String,Object> totalOutAndInByYear(@PathVariable(name = "year") int year){
+		
+		return cashStatsService.getTotalOutAndInByYear(year);
 	}
 }
